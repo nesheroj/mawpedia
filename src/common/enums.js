0a0292665bdf9f1da2abe02f07069b9229da23cd
Nestor Hernandez Ojeda <nestor.ojeda@adstream.com>
2016-05-02 23:10:00 +0100

    Fix Typo

--- src/common/enums.js
+++ src/common/enums.js
@@ -1,5 +1,5 @@
 export const cardTypes = ['Pantheon', 'Character', 'Gear', 'Resource', 'Action', 'Event', 'Summon', 'Token'];
 export const textTypes = ['Flavor', 'General', 'Influence', 'Confrontation', 'Lore', 'Notes'];
 export const cardExpansions = ['MaW1'];
-export const cardFactions = ['Japanese', 'Norse', 'Aztec', 'Greek', 'Egiptian', 'Neutral', 'Primigenic', 'Celtic', 'Sumerian', 'Hindu', 'Voodoo', 'Native american'];
+export const cardFactions = ['Japanese', 'Norse', 'Aztec', 'Greek', 'Egyptian', 'Neutral', 'Primigenic', 'Celtic', 'Sumerian', 'Hindu', 'Voodoo', 'Native american'];
 export const sortFields = ['Name', 'Code', 'Cost', 'Power', 'Strength', 'Type', 'Expansion', 'Faction'];
