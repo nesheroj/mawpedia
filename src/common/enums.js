12708abe924800a635542476f9a3759a9c60a763
nesukun <nesukun@gmail.com>
2017-07-09 14:27:18 +0100

    Add GDM12

--- src/common/enums.js
+++ src/common/enums.js
@@ -2,7 +2,7 @@ export const cardTypes = ['Pantheon', 'Character', 'Gear', 'Resource', 'Action',
 export const textTypes = ['Flavor', 'General', 'Influence', 'Confrontation', 'Lore', 'Notes'];
 export const cardExpansions = {
   mawpedia: ['MaW1', 'MaW2'],
-  mitopedia: ['GDM1', 'GDM2', 'GDM3', 'GDM4', 'GDM5', 'GDM6', 'GDM7', 'GDM8', 'GDM9', 'GDM10', 'GDM11', 'Promocional']
+  mitopedia: ['GDM1', 'GDM2', 'GDM3', 'GDM4', 'GDM5', 'GDM6', 'GDM7', 'GDM8', 'GDM9', 'GDM10', 'GDM11', 'GDM12', 'Promocional']
 };
 export const hiddenExpansions = {
   mawpedia: ['MaW2'],
@@ -11,6 +11,6 @@ export const hiddenExpansions = {
 export const cardFactions = ['Japanese', 'Norse', 'Aztec', 'Greek', 'Egyptian', 'Neutral', 'Primigenic', 'Celtic', 'Sumerian', 'Hindu', 'Voodoo', 'Native american', 'Roman', 'Persian', 'Chinese'];
 export const hiddenFactions = {
   mawpedia: ['Celtic', 'Sumerian', 'Hindu', 'Voodoo', 'Native american', 'Roman', 'Persian', 'Chinese'],
-  mitopedia: ['Chinese']
+  mitopedia: []
 };
 export const sortFields = ['Name', 'Code', 'Cost', 'Power', 'Strength', 'Type', 'Expansion', 'Faction'];
