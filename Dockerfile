49a7022e414faf029f92f335fe5c66215f9e7925
nesukun <nesukun@gmail.com>
2018-06-20 00:37:04 +0100

    asd

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,40 @@
-FROM node:6-slim
+FROM node:10 AS build
 
 # Shut up meg
-ENV NPM_CONFIG_LOGLEVEL warn
+ENV NPM_CONFIG_LOGLEVEL warn \
+    NODE_ENV=production
 
 # Create app directory
-RUN mkdir -p /usr/src/app
-WORKDIR /usr/src/app
+RUN mkdir /app
+WORKDIR /app
 
-# Install app dependencies
-COPY package.json /usr/src/app/
-RUN npm install --production=false; exit 0
+# Copy source
+COPY . ./
 
-# Bundle app source
-COPY . /usr/src/app
+# Install all dependencies
+RUN npm install --production=false
+
+RUN npm test && npm run lint
+
+RUN npm run build:prod
+
+RUN npm prune --production
+
+FROM node:10-alpine as run
+
+ENV NODE_ENV=production
+
+# Create app directory
+RUN mkdir /app
+WORKDIR /app
+
+COPY --from=build --chown=node:node ["/app/bin", "./bin"]
+COPY --from=build --chown=node:node ["/app/dist", "./dist"]
+COPY --from=build --chown=node:node ["/app/node_modules", "./node_modules"]
+COPY --from=build --chown=node:node ["/app/package.json", "./"]
+
+USER node
 
 EXPOSE 8080
+
 CMD [ "npm", "start" ]
--- webpack.config.js
+++ webpack.config.js
@@ -1,116 +1,221 @@
-import path from 'path';
-import autoprefixer from 'autoprefixer';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-import StatsPlugin from 'stats-webpack-plugin';
-import webpack from 'webpack';
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const postcssOptions = {
-  plugins: () => [
-    autoprefixer({ browsers: ['last 2 versions'] })
-  ]
-};
-
-const config = {
-  cache: false,
-  context: path.resolve('./src'),
-  devtool: 'eval',
-  entry: [
-    './client/index.js'
-  ],
-  output: {
-    filename: 'main.js?[hash]',
-    path: path.resolve('./static')
+module.exports = {
+  entry: {
+    bundle: './src/client/index.js',
   },
   module: {
-    rules: [{
-      test: /\.js$/i,
-      exclude: /node_modules/i,
-      loader: 'babel-loader',
-      query: {
-        plugins: ['transform-runtime', 'transform-class-properties', 'transform-decorators-legacy'],
-        presets: [['env', {
-          modules: false,
-          targets: {
-            browsers: 'last 2 versions'
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src'),
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              forceEnv: 'browser',
+            },
           },
-          useBuiltIns: true
-        }], 'stage-3']
-      }
-    }, {
-      test: /\.json$/i,
-      loader: 'json-loader'
-    }, {
-      test: /\.html$/i,
-      loader: 'html-loader'
-    }, {
-      test: /\.css$/i,
-      loaders: [
-        'style-loader',
-        'css-loader'
-      ]
-    }, {
-      test: /\.scss$/i,
-      exclude: [
-        /src(\\|\/)+client(\\|\/)+index\.scss$/i
-      ],
-      loaders: [
-        'raw-loader',
-        {
-          loader: 'postcss-loader',
-          options: postcssOptions
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: () => [require('autoprefixer')()],
+            },
+          },
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: () => [require('autoprefixer')()],
+            },
+          },
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.html$/,
+        use: 'html-loader',
+      },
+      // {
+      //   test: /\.(gif|png|jpe?g)$/,
+      //   include: path.resolve(__dirname, 'src/public/images'),
+      //   use: ['file-loader?name=images/[name].[ext]'],
+      // },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        include: /tesco-modern-fonts/,
+        use: ['file-loader?name=fonts/[name].[ext]'],
+      },
+      {
+        test: /\.svg$/,
+        exclude: /tesco-modern-fonts/,
+        use: ['file-loader?name=svg/[name].[ext]'],
+      },
+    ],
+  },
+  output: {
+    path: path.join(__dirname, 'dist', 'static'),
+    publicPath: '/data-portability/',
+    filename: '[name]-[chunkhash].js',
+  },
+  mode: process.env.NODE_ENV,
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        styles: {
+          name: 'bundle',
+          test: /\.scss$/,
+          chunks: 'all',
+          enforce: true,
         },
-        'sass-loader'
-      ]
-    }, {
-      test: /\.scss$/i,
-      include: [
-        /src(\\|\/)+client(\\|\/)+index\.scss$/i
-      ],
-      loaders: [
-        'style-loader',
-        {
-          loader: 'css-loader',
-          options: { importLoaders: 1 }
-        },
-        // {
-        //   loader: 'postcss-loader',
-        //   options: postcssOptions
-        // },
-        'sass-loader'
-      ]
-    }, {
-      test: /\.(jpe?g|png|gif)$/i,
-      loader: 'file-loader',
-      query: {
-        name: '[path][name].[ext]?[hash]'
-      }
-    }, {
-      test: /\.(ttf|woff|eot)$/i,
-      loader: 'file-loader',
-      query: {
-        name: '[path][name].[ext]?[hash]'
-      }
-    }]
+      },
+    },
   },
   plugins: [
     new HtmlWebpackPlugin({
       filename: 'index.html',
-      template: 'client/index.html'
+      template: 'src/client/index.html',
     }),
-    new webpack.DefinePlugin({
-      __DEVELOPMENT__,
-      __PRODUCTION__
-    }),
-    new StatsPlugin(path.join('..', 'webpack-stats.json'), {
-      chunkModules: true
-    })
-  ]
+  ],
 };
 
-if (__PRODUCTION__) {
+// import path from 'path';
+// import autoprefixer from 'autoprefixer';
+// import HtmlWebpackPlugin from 'html-webpack-plugin';
+// import StatsPlugin from 'stats-webpack-plugin';
+// import webpack from 'webpack';
 
-  config.plugins.push(new webpack.optimize.UglifyJsPlugin());
+// const postcssOptions = {
+//   plugins: () => [
+//     autoprefixer({ browsers: ['last 2 versions'] })
+//   ]
+// };
 
-}
+// const config = {
+//   cache: false,
+//   context: path.resolve('./src'),
+//   devtool: 'eval',
+//   entry: [
+//     './client/index.js'
+//   ],
+//   output: {
+//     filename: 'main.js?[hash]',
+//     path: path.resolve('./static')
+//   },
+//   module: {
+//     rules: [{
+//       test: /\.js$/i,
+//       exclude: /node_modules/i,
+//       loader: 'babel-loader',
+//       query: {
+//         plugins: ['transform-runtime', 'transform-class-properties', 'transform-decorators-legacy'],
+//         presets: [['env', {
+//           modules: false,
+//           targets: {
+//             browsers: 'last 2 versions'
+//           },
+//           useBuiltIns: true
+//         }], 'stage-3']
+//       }
+//     }, {
+//       test: /\.json$/i,
+//       loader: 'json-loader'
+//     }, {
+//       test: /\.html$/i,
+//       loader: 'html-loader'
+//     }, {
+//       test: /\.css$/i,
+//       loaders: [
+//         'style-loader',
+//         'css-loader'
+//       ]
+//     }, {
+//       test: /\.scss$/i,
+//       exclude: [
+//         /src(\\|\/)+client(\\|\/)+index\.scss$/i
+//       ],
+//       loaders: [
+//         'raw-loader',
+//         {
+//           loader: 'postcss-loader',
+//           options: postcssOptions
+//         },
+//         'sass-loader'
+//       ]
+//     }, {
+//       test: /\.scss$/i,
+//       include: [
+//         /src(\\|\/)+client(\\|\/)+index\.scss$/i
+//       ],
+//       loaders: [
+//         'style-loader',
+//         {
+//           loader: 'css-loader',
+//           options: { importLoaders: 1 }
+//         },
+//         // {
+//         //   loader: 'postcss-loader',
+//         //   options: postcssOptions
+//         // },
+//         'sass-loader'
+//       ]
+//     }, {
+//       test: /\.(jpe?g|png|gif)$/i,
+//       loader: 'file-loader',
+//       query: {
+//         name: '[path][name].[ext]?[hash]'
+//       }
+//     }, {
+//       test: /\.(ttf|woff|eot)$/i,
+//       loader: 'file-loader',
+//       query: {
+//         name: '[path][name].[ext]?[hash]'
+//       }
+//     }]
+//   },
+//   plugins: [
+//     new HtmlWebpackPlugin({
+//       filename: 'index.html',
+//       template: 'client/index.html'
+//     }),
+//     new webpack.DefinePlugin({
+//       __DEVELOPMENT__,
+//       __PRODUCTION__
+//     }),
+//     new StatsPlugin(path.join('..', 'webpack-stats.json'), {
+//       chunkModules: true
+//     })
+//   ]
+// };
 
-export default config;
+// if (__PRODUCTION__) {
+
+//   config.plugins.push(new webpack.optimize.UglifyJsPlugin());
+
+// }
+
+// export default config;
